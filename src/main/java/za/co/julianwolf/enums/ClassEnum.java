daf12429534660287732265ef2df440b882e016f
Julian Wolf <julian.w16@gmail.com>
2019-07-22 14:57:44 +0200

    auto commit

---
+++ src/main/java/za/co/julianwolf/enums/ClassEnum.java
@@ -0,0 +1,13 @@
+package za.co.julianwolf.enums;
+
+public enum  ClassEnum {
+    NONE,
+    BASIC_HERO,
+    ARCHER,
+    WIZARD,
+    NINEGAGER,
+    SLACKER,
+    LORD,
+    KING,
+    PLEB
+}
---
+++ src/main/java/za/co/julianwolf/interfaces/State.java
@@ -0,0 +1,12 @@
+package za.co.julianwolf.interfaces;
+
+public enum State {
+    NONE,
+    IDLE,
+    MOVING,
+    ATTACKING,
+    DEFENDING,
+    INVINCIBLE,
+    LEVELING,
+    DEAD
+}
